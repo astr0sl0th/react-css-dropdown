4ff4002fafc63c23a0be2d547a3175b4fe243e60
Joe Hill <joe@adultcms.co>
2020-07-31 08:03:32 +0100

    Updates

---
+++ precache-manifest.3303cef501d7bbf0aa5c2398638dcf8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126c9a2afceb6c1b69a57da2ca72fd4c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d96bfa398f41cd279fd0",
+    "url": "./static/css/main.6e60e120.chunk.css"
+  },
+  {
+    "revision": "c0caedd976802e18a355",
+    "url": "./static/js/2.066ed865.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.066ed865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96bfa398f41cd279fd0",
+    "url": "./static/js/main.073ada8c.chunk.js"
+  },
+  {
+    "revision": "253289a9dc731906f17d",
+    "url": "./static/js/runtime-main.9b96c8ec.js"
+  }
+]);
